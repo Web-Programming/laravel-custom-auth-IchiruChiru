1171c5786e5740ffa7e40452c75bea966cbce363
IchiruChiru <richiejonathanchaniago@gmail.com>
2023-11-29 13:10:19 +0700

    au

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
@@ -31,6 +32,7 @@ Route::post('proses_register', [AuthController::class, 'proses_register'])->name
 // didalamnya terdapat group untuk mengecek kondisi login
 // jika user yang login merupakan admin maka akan diarahkan ke AdminController
 // jika user yang login merupakan user biasa maka akan diarahkan ke UserController
+
 Route::group(['middleware' => ['auth']], function () {
     Route::group(['middleware' => ['cek_login:admin']], function () {
         Route::resource('admin', AdminController::class);
@@ -38,4 +40,24 @@ Route::group(['middleware' => ['auth']], function () {
     Route::group(['middleware' => ['cek_login:user']], function () {
         Route::resource('user', UserController::class);
     });
+
+    Route::get('/prodi', [ProdiController::class, 'index']);
+    Route::get('/prodi/create', [ProdiController::class, 'create'])->name('prodi.create');
+    Route::post('/prodi/store', [ProdiController::class, 'store']);
+
+
+    Route::get('/prodi/all-join-facade', [ProdiController::class, 'allJoinFacade']);
+    Route::get('/prodi/all-join-elq', [ProdiController::class, 'allJoinElq']);
+
+
+    Route::get('/prodi', [ProdiController::class, 'index'])->name('prodi.index');
+    Route::get('/prodi/{prodi}', [ProdiController::class, 'show'])->name('prodi.show');
+
+    Route::get('/prodi/{prodi}/edit', [ProdiController::class, 'edit'])->name('prodi.edit');
+    Route::patch('/prodi/{prodi}', [ProdiController::class, 'update'])->name('prodi.update');
+
+    Route::delete('/prodi/{prodi}', [ProdiController::class, 'destroy'])->name('prodi.destroy');
 });
+
+
+
